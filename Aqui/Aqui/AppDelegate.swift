4b2a7155e5ca907436ffd845e58a102f18e4d0aa
Dara adekore <adekore8@gmail.com>
2022-11-15 16:06:51 -0800

    - Made Schemas Visible

--- Aqui/Aqui/AppDelegate.swift
+++ Aqui/Aqui/AppDelegate.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
-
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AppCenter.start(withAppSecret: "{Your app secret here}", services:[
+          Analytics.self,
+          Crashes.self
+        ])
         return true
     }
 
